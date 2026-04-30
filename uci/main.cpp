95cc0f56c51a98fcd3c8616d1545494abfee4507
Stanislaw Dabrowski <dabrowski.stanislaw3141@gmail.com>
2026-04-30 13:31:54 +0200

    added asynchronous command execution

--- uci/main.cpp
+++ uci/main.cpp
@@ -1,12 +1,18 @@
 #include "Engine.h"
 #include "Utils.h"
+#include <ostream>
 #include <unordered_map>
 #include <functional>
+#include <thread>
+#include <atomic>
+#include <syncstream>
 
 
 Engine engine = Engine();
+Board board_copy;
+std::mutex engine_mutex;
 
-bool debug_mode = false;
+std::atomic<bool> debug_mode = false;
 
 void do_nothing_command_function(std::vector<std::string> args)
 {
@@ -15,59 +21,60 @@ void do_nothing_command_function(std::vector<std::string> args)
 
 void d_command_function(std::vector<std::string> args)
 {
-	Utils::display_board(&engine.board, std::cout);
-}
-
-void exit_command_function(std::vector<std::string> args)
-{
-	exit(0);
+	std::osyncstream out(std::cout);//necessary to pass the out as an lvalue not rvalue
+	Utils::display_board(&board_copy, out);
 }
 
 void isready_command_function(std::vector<std::string> args)
 {
-	std::cout << "readyok" << std::endl;
+	std::osyncstream out(std::cout);
+	out << "readyok" << std::endl;
 }
 
 void uci_command_function(std::vector<std::string> args)
 {
-	std::cout << "id name ChessEngine2" << std::endl;
-	std::cout << "id author Avalfortz" << std::endl;
-	std::cout << "uciok" << std::endl;
+	std::osyncstream out(std::cout);
+	out << "id name ChessEngine2" << std::endl;
+	out << "id author Avalfortz" << std::endl;
+	out << "uciok" << std::endl;
 }
 
 void debug_command_function(std::vector<std::string> args)
 {
+	std::osyncstream out(std::cout);
 	bool value_provided = false;
 	for (int i = 0;i<args.size();++i)
 	{
 		if (args[i]=="on")
 		{
-			debug_mode = true;
+			debug_mode.store(true, std::memory_order_relaxed);
 			value_provided = true;
 			break;
 		}
 		else if (args[i]=="off")
 		{
-			debug_mode = false;
+			debug_mode.store(false, std::memory_order_relaxed);
 			value_provided = true;
 			break;
 		}
 	}
 	if (!value_provided)
 	{
-		std::cout << "No value provided for debug command. Use 'debug on' or 'debug off'." << std::endl;
+		out << "No value provided for debug command. Use 'debug on' or 'debug off'." << std::endl;
 		return;
 	}
-	std::cout << "Debug mode " << (debug_mode ? "enabled" : "disabled") << std::endl;
+	out << "Debug mode " << (debug_mode.load(std::memory_order_relaxed) ? "enabled" : "disabled") << std::endl;
 }
 
 
 void go_command_function(std::vector<std::string> args)
 {
-
+	std::osyncstream out(std::cout);
+	out << std::emit_on_flush;
 	if (args.size() >= 2 && args[0]=="perft")
 	{
 		uint8_t depth = std::stoi(args[1]);
+		engine_mutex.lock();
 		if (engine.board.side_to_move == White)
 		{
 			engine.mg.generate_pseudo_legal_moves<White>();
@@ -95,10 +102,11 @@ void go_command_function(std::vector<std::string> args)
 			else
 				perft_result = 1;
 			count += perft_result;
-			std::cout << Utils::move_to_string(move) << ": " << perft_result << std::endl;
+			out << Utils::move_to_string(move) << ": " << perft_result << std::endl;
 			engine.board.unmake_move();
 		}
-		std::cout << count << std::endl;
+		engine_mutex.unlock();
+		out << count << std::endl;
 	}
 	else
 	{
@@ -114,46 +122,51 @@ void go_command_function(std::vector<std::string> args)
 				}
 				else
 				{
-					std::cout << "No depth specified after 'depth'" << std::endl;
+					out << "No depth specified after 'depth'" << std::endl;
 					return;
 				}
 			}
 		}
 		if (depth == -1)
 		{
-			std::cout << "No depth specified" << std::endl;
+			out << "No depth specified" << std::endl;
 			return;
 		}
 		std::pair<Move, int16_t> search_result;
-		if (debug_mode)
+		engine_mutex.lock();
+		bool debug_mode_local = debug_mode.load(std::memory_order_relaxed);//made to avoid debug_mode changing in the middle of the search causing inconsistent debug output
+		if (debug_mode_local)
 			engine.nodes_searched = 0;
 		if (engine.board.side_to_move == White)
 		{
-			if (debug_mode)
+			if (debug_mode_local)
 				search_result = engine.search<White, true, true>(depth);
 			else
 				search_result = engine.search<White, true, false>(depth);
 		}
 		else
 		{
-			if (debug_mode)
+			if (debug_mode_local)
 				search_result = engine.search<Black, true, true>(depth);
 			else
 				search_result = engine.search<Black, true, false>(depth);
 		}
-		std::cout << "bestmove " << Utils::move_to_string(search_result.first) << std::endl;
-		std::cout << "score " << search_result.second << std::endl;
-		if (debug_mode)
+		out << "bestmove " << Utils::move_to_string(search_result.first) << std::endl;
+		out << "score " << search_result.second << std::endl;
+		if (debug_mode_local)
 		{
-			std::cout << "nodes searched: " << engine.nodes_searched << std::endl;
+			out << "nodes searched: " << engine.nodes_searched << std::endl;
 			engine.nodes_searched = 0;
 		}
+		engine_mutex.unlock();
 
 
 	}
 }
 void position_command_function(std::vector<std::string> args)
-{ for (int i = 0;i<args.size();++i)
+{
+	engine_mutex.lock();
+	for (int i = 0;i<args.size();++i)
 	{
 		if (args[i]=="fen")
 		{
@@ -172,10 +185,12 @@ void position_command_function(std::vector<std::string> args)
 				Move move_temp = Utils::string_to_move(&engine.board, args[i]);
 				engine.board.make_move(move_temp);
 			}
-			return;
+			break;
 		}
 		
 	}
+	board_copy = engine.board;
+	engine_mutex.unlock();
 }
 
 std::vector<std::string> tokenize(const std::string& input)
@@ -217,12 +232,9 @@ std::vector<std::string> tokenize(const std::string& input)
 int main()
 {
 	Utils::initialize_board(&engine.board);
+	board_copy = engine.board;
 	std::unordered_map<std::string, std::function<void(std::vector<std::string>)>> commands_functions = {
 		{"d", d_command_function},
-		{"exit", exit_command_function},
-		{"e", exit_command_function},
-		{"quit", exit_command_function},
-		{"q", exit_command_function},
 		{"go", go_command_function},
 		{"position", position_command_function},
 		{"isready", isready_command_function},
@@ -231,7 +243,7 @@ int main()
 		{"ucinewgame", do_nothing_command_function},
 	};
 	
-
+	std::osyncstream out(std::cout);
 	std::string input_line;
 	std::vector<std::string> tokens;
 	while (true)
@@ -241,16 +253,20 @@ int main()
 		tokens = tokenize(input_line);
 		if (tokens.empty())
 			continue;
+		if (tokens[0] == "exit" || tokens[0] == "e" || tokens[0] == "quit" || tokens[0] == "q")
+		{
+			break;//exit
+		}
 		if (commands_functions.contains(tokens[0]))
 		{
-			commands_functions[tokens[0]](std::vector<std::string>(tokens.begin()+1, tokens.end()));
+			std::thread t(commands_functions[tokens[0]], std::vector<std::string>(tokens.begin()+1, tokens.end()));
+			t.detach();
 		}
 		else
 		{
-			std::cout << "Unknown command: " << tokens[0] << std::endl;
+			out << "Unknown command: " << tokens[0] << std::endl;
 			continue;
 		}
-
 	}
 
 	return 0;
