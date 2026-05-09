c70f77d3ffa23e9961df9c31007171f891d864b2
Stanislaw Dabrowski <dabrowski.stanislaw3141@gmail.com>
2026-05-09 19:15:35 +0200

    fixed issue with function calculating mobility score calculating absolut, not relative score

--- engine/StaticEval.cpp
+++ engine/StaticEval.cpp
@@ -16,7 +16,6 @@ int16_t StaticEval::evaluate_mobility()
 	//mobility is calculated as number of pseudo legal moves but also couning attacks on pieces of the same color and for pawns also squares they can attack even when there are no pieces, because of that captures for pawns are divided by 2
 	Color opp = color == White ? Black : White;
 	int16_t score = 0;
-	int16_t absolute_mobility_score = color == White ? mobility_score : -mobility_score;//non relative score for mobility
 
 	Bitboard piece_copy = board->positions_stack[board->current_position_idx].pieces[color][Pawn];
 
@@ -32,9 +31,9 @@ int16_t StaticEval::evaluate_mobility()
 	Bitboard left_capture_promotion = (color == White ? ((piece_copy & Utils::FILE_A_NEGATION & Utils::RANK_7) << 7) : ((piece_copy & Utils::FILE_A_NEGATION & Utils::RANK_2) >> 9));
 	Bitboard right_capture_promotion = (color == White ? ((piece_copy & Utils::FILE_H_NEGATION & Utils::RANK_7) << 9) : ((piece_copy & Utils::FILE_H_NEGATION & Utils::RANK_2) >> 7));
 
-	score += (std::popcount(single_push) + std::popcount(double_push) + std::popcount(push_promotion)) * absolute_mobility_score;
-	score += ((std::popcount(left_captures) + std::popcount(right_captures) + std::popcount(left_capture_promotion) + std::popcount(right_capture_promotion)) * absolute_mobility_score) >> 1;//capture posibilities are worth less beacuse moving there without a capture is not possible and most of these are not actually possible at the moment, there is not piece to capture. this is only a special case of pawns
-	score += (std::popcount(single_push) + std::popcount(double_push) + std::popcount(left_captures) + std::popcount(right_captures) + std::popcount(push_promotion) + std::popcount(left_capture_promotion) + std::popcount(right_capture_promotion)) * absolute_mobility_score;
+	score += (std::popcount(single_push) + std::popcount(double_push) + std::popcount(push_promotion)) * mobility_score;
+	score += ((std::popcount(left_captures) + std::popcount(right_captures) + std::popcount(left_capture_promotion) + std::popcount(right_capture_promotion)) * mobility_score) >> 1;//capture posibilities are worth less beacuse moving there without a capture is not possible and most of these are not actually possible at the moment, there is not piece to capture. this is only a special case of pawns
+	score += (std::popcount(single_push) + std::popcount(double_push) + std::popcount(left_captures) + std::popcount(right_captures) + std::popcount(push_promotion) + std::popcount(left_capture_promotion) + std::popcount(right_capture_promotion)) * mobility_score;
 
 	//en passant
 	uint8_t en_passant_square = board->positions_stack[board->current_position_idx].en_passant_square;
@@ -55,11 +54,11 @@ int16_t StaticEval::evaluate_mobility()
 	}
 	if (pawns_left & en_passant_mask)
 	{
-		score += absolute_mobility_score;
+		score += mobility_score;
 	}
 	if (pawns_right & en_passant_mask)
 	{
-		score += absolute_mobility_score;
+		score += mobility_score;
 	}
 
 	size_t from;
@@ -70,7 +69,7 @@ int16_t StaticEval::evaluate_mobility()
 	{
 		from = std::countr_zero(piece_copy);
 		attacks = mg->knight_attack_tables[from];
-		score += std::popcount(attacks) * absolute_mobility_score;
+		score += std::popcount(attacks) * mobility_score;
 		piece_copy &= piece_copy - 1;
 	}
 	//bishops
@@ -83,7 +82,7 @@ int16_t StaticEval::evaluate_mobility()
 		relevant_blockers = board->positions_stack[board->current_position_idx].all_pieces & mg->bishop_relevant_blockers[from];
 		index = (relevant_blockers * mg->bishop_magic_numbers[from]) >> (64 - std::popcount(mg->bishop_relevant_blockers[from]));
 		attacks = mg->bishop_attack_tables[from][index];
-		score += std::popcount(attacks) * absolute_mobility_score;
+		score += std::popcount(attacks) * mobility_score;
 		piece_copy &= piece_copy - 1;
 	}
 	//rooks
@@ -94,7 +93,7 @@ int16_t StaticEval::evaluate_mobility()
 		relevant_blockers = board->positions_stack[board->current_position_idx].all_pieces & mg->rook_relevant_blockers[from];
 		index = (relevant_blockers * mg->rook_magic_numbers[from]) >> (64 - std::popcount(mg->rook_relevant_blockers[from]));
 		attacks = mg->rook_attack_tables[from][index];
-		score += std::popcount(attacks) * absolute_mobility_score;
+		score += std::popcount(attacks) * mobility_score;
 		piece_copy &= piece_copy - 1;
 	}
 	//queens
@@ -108,14 +107,14 @@ int16_t StaticEval::evaluate_mobility()
 		relevant_blockers = board->positions_stack[board->current_position_idx].all_pieces & mg->rook_relevant_blockers[from];
 		index = (relevant_blockers * mg->rook_magic_numbers[from]) >> (64 - std::popcount(mg->rook_relevant_blockers[from]));
 		attacks |= mg->rook_attack_tables[from][index];
-		score += std::popcount(attacks) * absolute_mobility_score;
+		score += std::popcount(attacks) * mobility_score;
 		piece_copy &= piece_copy - 1;
 	}
 	//king
 	piece_copy = board->positions_stack[board->current_position_idx].pieces[color][King];
 	from = std::countr_zero(piece_copy);
 	attacks = mg->king_attack_tables[from];
-	score += std::popcount(attacks) * absolute_mobility_score;
+	score += std::popcount(attacks) * mobility_score;
 
 	return score;
 }
